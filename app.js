b2097350307227a312655b11ac40144ca4a1320b
faraaz baig <fazzy0908@yandex.com>
2021-02-28 20:42:08 +0530

    using view to make dummy blog

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-const express = require('express')
+const express = require("express");
 
 // express app
-const app = express()
+const app = express();
+
+//register view engine
+app.set("view engine", "ejs");
 
 // listen for requests
-app.listen(3000)
-console.log('listening on port 3000')
+app.listen(3000);
+console.log("listening on port 3000");
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
-})
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
+});
 
-app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
-})
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
+});
 
 // 404 page (always at the bottom of the scope)
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+  res.status(404).render("404", { title: "Error 404" });
+});
